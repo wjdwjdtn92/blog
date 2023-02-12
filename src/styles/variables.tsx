f7e58cf2d624f88961978c56762a1f50d3210fbd
Jung Su <wjdwjdtn92@naver.com>
2023-02-12 21:41:05 +0900

    💄 (#1)CSS color 상수 추가

---
+++ src/styles/variables.tsx
@@ -0,0 +1,18 @@
+import { css } from 'styled-components';
+
+const variables = css`
+  :root {
+    --black: #000;
+    --dark: #22252a;
+    --white: #fff;
+    --border: #292c33;
+    --light-gray: #f6f6f6;
+    --background: #292c33;
+
+    --red: #ed6a5e;
+    --grey: #d5d5d5;
+    --green: #61c454;
+  }
+`;
+
+export default variables;
